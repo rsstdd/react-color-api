068d7b6c74de97019d8f14ff95087d2789e2fc03
rsstdd <rosstod@gmail.com>
2017-09-20 18:55:04 -0700

    Fix seeds

---
+++ api/knex/seeds/01_colors.js
@@ -0,0 +1,527 @@
+exports.seed = function (knex) {
+  return knex('colors').del()
+    .then(() => {
+      return knex('colors').insert([
+        {
+          id: 1,
+          hex: '#508584',
+          color: 'blue'
+        },
+        {
+          id: 2,
+          hex: '#c41e3a',
+          color: 'red'
+        },
+        {
+          id: 3,
+          hex: '#0085ca',
+          color: 'blue'
+        },
+        {
+          id: 4,
+          hex: '#bad80a',
+          color: 'green'
+        },
+        {
+          id: 5,
+          hex: '#ef6b00',
+          color: 'orange'
+        },
+        {
+          id: 6,
+          hex: '#7369b0',
+          color: 'purple'
+        },
+        {
+          id: 7,
+          hex: '#2d3f58',
+          color: 'blue'
+        },
+        {
+          id: 8,
+          hex: '#dadcac',
+          color: 'green'
+        },
+        {
+          id: 9,
+          hex: '#ffb6b9',
+          color: 'red'
+        },
+        {
+          id: 10,
+          hex: '#fbeee5',
+          color: 'white'
+        },
+        {
+          id: 11,
+          hex: '#dla827',
+          color: 'yellow'
+        },
+        {
+          id: 12,
+          hex: '#3c455c',
+          color: 'blue'
+        },
+        {
+          id: 13,
+          hex: '#f5ab99',
+          color: 'red'
+        },
+        {
+          id: 14,
+          hex: '#0e2f44',
+          color: 'blue'
+        },
+        {
+          id: 15,
+          hex: '#8aa4ab',
+          color: 'blue'
+        },
+        {
+          id: 16,
+          hex: '#314036',
+          color: 'green'
+        },
+        {
+          id: 17,
+          hex: '#008080',
+          color: 'blue'
+        },
+        {
+          id: 18,
+          hex: '#ffd700',
+          color: 'yellow'
+        },
+        {
+          id: 19,
+          hex: '#ffa500',
+          color: 'yellow'
+        },
+        {
+          id: 20,
+          hex: '#1c1c1c',
+          color: 'gray'
+        },
+        {
+          id: 21,
+          hex: '#ec4700',
+          color: 'orange'
+        },
+        {
+          id: 22,
+          hex: '#fb5923',
+          color: 'orange'
+        },
+        {
+          id: 23,
+          hex: '#631939',
+          color: 'yellow'
+        },
+        {
+          id: 24,
+          hex: '#e5dcb6',
+          color: 'cream'
+        },
+        {
+          id: 25,
+          hex: '#a2c0c3',
+          color: 'blue'
+        },
+        {
+          id: 26,
+          hex: '#7f2e12',
+          color: 'orange'
+        },
+        {
+          id: 27,
+          hex: '#e7a250',
+          color: 'yellow'
+        },
+        {
+          id: 28,
+          hex: '#f2ba0d',
+          color: 'yellow'
+        },
+        {
+          id: 29,
+          hex: '#fef6c9',
+          color: 'cream'
+        },
+        {
+          id: 30,
+          hex: '#fd41ff',
+          color: 'purple'
+        },
+        {
+          id: 31,
+          hex: '#1e112f',
+          color: 'purple'
+        },
+        {
+          id: 32,
+          hex: '#30efff',
+          color: 'blue'
+        },
+        {
+          id: 33,
+          hex: '#0e221f',
+          color: 'green'
+        },
+        {
+          id: 34,
+          hex: '#7cff00',
+          color: 'green'
+        },
+        {
+          id: 35,
+          hex: '#381e15',
+          color: 'brown'
+        },
+        {
+          id: 36,
+          hex: '#ee6131',
+          color: 'orange'
+        },
+        {
+          id: 37,
+          hex: '#f6eb7f',
+          color: 'yellow'
+        },
+        {
+          id: 38,
+          hex: '#859c76',
+          color: 'green'
+        },
+        {
+          id: 39,
+          hex: '#a97c63',
+          color: 'brown'
+        },
+        {
+          id: 40,
+          hex: '#135358',
+          color: 'blue'
+        },
+        {
+          id: 41,
+          hex: '#1a454b',
+          color: 'blue'
+        },
+        {
+          id: 42,
+          hex: '#17403d',
+          color: 'green'
+        },
+        {
+          id: 43,
+          hex: '#101727',
+          color: 'blue'
+        },
+        {
+          id: 44,
+          hex: '#110b22',
+          color: 'blue'
+        },
+        {
+          id: 45,
+          hex: '#000000',
+          color: 'black'
+        },
+        {
+          id: 46,
+          hex: '#e38489',
+          color: 'red'
+        },
+        {
+          id: 47,
+          hex: '#b24148',
+          color: 'red'
+        },
+        {
+          id: 48,
+          hex: '#585c6f',
+          color: 'blue'
+        },
+        {
+          id: 49,
+          hex: '#68868b',
+          color: 'blue'
+        },
+        {
+          id: 50,
+          hex: '#6cbd2e',
+          color: 'green'
+        },
+        {
+          id: 51,
+          hex: '#38654f',
+          color: 'green'
+        },
+        {
+          id: 52,
+          hex: '#d04e2e',
+          color: 'orange'
+        },
+        {
+          id: 53,
+          hex: '#ba19ce',
+          color: 'purple'
+        },
+        {
+          id: 54,
+          hex: '#130f11',
+          color: 'brown'
+        },
+        {
+          id: 55,
+          hex: '#f4d7dd',
+          color: 'red'
+        },
+        {
+          id: 56,
+          hex: '#eca5b0',
+          color: 'red'
+        },
+        {
+          id: 57,
+          hex: '#db6f76',
+          color: 'red'
+        },
+        {
+          id: 58,
+          hex: '#ba3434',
+          color: 'red'
+        },
+        {
+          id: 59,
+          hex: '#b7bd65',
+          color: 'yellow'
+        },
+        {
+          id: 60,
+          hex: '#ae9254',
+          color: 'yellow'
+        },
+        {
+          id: 61,
+          hex: '#a56743',
+          color: 'orange'
+        },
+        {
+          id: 62,
+          hex: '#9c3c33',
+          color: 'orange'
+        },
+        {
+          id: 63,
+          hex: '#941021',
+          color: 'red'
+        },
+        {
+          id: 64,
+          hex: '#71c8d0',
+          color: 'yellow'
+        },
+        {
+          id: 65,
+          hex: '#88c9c3',
+          color: 'blue'
+        },
+        {
+          id: 66,
+          hex: '#a3ceb9',
+          color: 'green'
+        },
+        {
+          id: 67,
+          hex: '#bcd0ae',
+          color: 'green'
+        },
+        {
+          id: 68,
+          hex: '#d4d2a1',
+          color: 'green'
+        },
+        {
+          id: 69,
+          hex: '#00070d',
+          color: 'blue'
+        },
+        {
+          id: 70,
+          hex: '#041620',
+          color: 'blue'
+        },
+        {
+          id: 71,
+          hex: '#0b202d',
+          color: 'blue'
+        },
+        {
+          id: 72,
+          hex: '#244751',
+          color: 'blue'
+        },
+        {
+          id: 73,
+          hex: '#651414',
+          color: 'red'
+        },
+        {
+          id: 74,
+          hex: '##8ed857',
+          color: 'green'
+        },
+        {
+          id: 75,
+          hex: '#c77d26',
+          color: 'yellow'
+        },
+        {
+          id: 76,
+          hex: '#894622',
+          color: 'orange'
+        },
+        {
+          id: 77,
+          hex: '#542b28',
+          color: 'brown'
+        },
+        {
+          id: 78,
+          hex: '#2c263a',
+          color: 'gray'
+        },
+        {
+          id: 79,
+          hex: '#f9bb35',
+          color: 'yellow'
+        },
+        {
+          id: 80,
+          hex: '#e58d8d',
+          color: 'red'
+        },
+        {
+          id: 81,
+          hex: '#21b503',
+          color: 'green'
+        },
+        {
+          id: 82,
+          hex: '#79053b',
+          color: 'purple'
+        },
+        {
+          id: 83,
+          hex: '#2c273a',
+          color: 'gray'
+        },
+        {
+          id: 84,
+          hex: '#007474',
+          color: 'blue'
+        },
+        {
+          id: 85,
+          hex: '#33675a',
+          color: 'green'
+        },
+        {
+          id: 86,
+          hex: '#655b3f',
+          color: 'gray'
+        },
+        {
+          id: 87,
+          hex: '#984e25',
+          color: 'orange'
+        },
+        {
+          id: 88,
+          hex: '#cb410b',
+          color: 'orange'
+        },
+        {
+          id: 89,
+          hex: '#003153',
+          color: 'blue'
+        },
+        {
+          id: 90,
+          hex: '#345d4b',
+          color: 'green'
+        },
+        {
+          id: 91,
+          hex: '#688a42',
+          color: 'gray'
+        },
+        {
+          id: 92,
+          hex: '#9db63a',
+          color: 'yellow'
+        },
+        {
+          id: 93,
+          hex: '#d1e230',
+          color: 'yellow'
+        },
+        {
+          id: 94,
+          hex: '#fe6f5e',
+          color: 'red'
+        },
+        {
+          id: 95,
+          hex: '#ed8880',
+          color: 'red'
+        },
+        {
+          id: 96,
+          hex: '#dda1a2',
+          color: 'red'
+        },
+        {
+          id: 97,
+          hex: '#cdbbc4',
+          color: 'red'
+        },
+        {
+          id: 98,
+          hex: '#bcd4e6',
+          color: 'blue'
+        },
+        {
+          id: 99,
+          hex: '#c53f05',
+          color: 'red'
+        },
+        {
+          id: 100,
+          hex: '#d1e231',
+          color: 'yellow'
+        },
+        {
+          id: 101,
+          hex: '#1e093a',
+          color: 'purple'
+        },
+        {
+          id: 102,
+          hex: '#1f2f17',
+          color: 'green'
+        },
+        {
+          id: 103,
+          hex: '#00e197',
+          color: 'green'
+        },
+      ]);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('colors_id_seq', (SELECT MAX(id) FROM colors));"
+      );
+    });
+};
